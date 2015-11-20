20745482cc1b705690c9991e27ca773fb24daec9
David Fluck <fluck.d@gmail.com>
2015-11-20 11:00:57 -0500

    Added most of the remaining functionality of the Markov generator. Made it possible to parse command line filename arguments as well as pass through an order argument.
    Also added a README.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Python Markov Chain Generator
+Just another Markov chain generator written in Python.
+
+# Usage
+`python main.py filenames [filenames ...]`
--- main.py
+++ main.py
@@ -1,20 +1,22 @@
+import argparse
 import random
 
 # Return a list of the next "words" from a file. The resulting list's size is determined by the order argument.
-def get_words(order = 3):
+def get_words(filenames, order):
     buffer = []
-    with open("corpus.txt", "r") as f:
-        for line in f:
-            lines = line.split()
-            buffer.extend(lines)
-            while(len(buffer) > order):
-                yield buffer[0:order]
-                buffer.pop(0)
+    for file in filenames:
+        with open(file, 'r') as f:
+            for line in f:
+                lines = line.split()
+                buffer.extend(lines)
+                while(len(buffer) > order):
+                    yield buffer[0:order]
+                    buffer.pop(0)
 
-def populate_table():
-    # Populate a dictionary with key/value pairs of words and their respective frequencies.
+def populate_table(filenames, order):
+    # Populate a dictionary with key/value pairs of words and the word that follows immediately after.
     markov_table = {}
-    for x in get_words(3):
+    for x in get_words(filenames, order):
         key = tuple(x[:-1])
         if markov_table.get(key):
             # We've seen this n-gram before, so append the following word to the associated list in the hash.
@@ -23,5 +25,29 @@ def populate_table():
             # This is a new n-gram.
             markov_table[key] = [x[-1]]
 
-def markov():
-    markov_table = populate_table()
+    return markov_table
+
+def markov(filenames, order = 4):
+    markov_table = populate_table(filenames, order)
+
+    # Choose a random starting state and next word.
+    random_state = random.choice(list(markov_table.keys()))
+    initial_next_word = random.choice(markov_table[random_state])
+
+    word_list = list(random_state) + [initial_next_word]
+    print(' '.join(word_list), end=' ')
+
+    for x in range(1,500):
+        # Choose the next word.
+        next_state = tuple(word_list[-(order - 1):])
+        next_word = random.choice(markov_table[next_state])
+
+        word_list = list(next_state) + [next_word]
+        print("{} ".format(next_word), end='')
+
+# Handle command-line parsing.
+parser = argparse.ArgumentParser()
+parser.add_argument('filenames', nargs='+')
+args = parser.parse_args()
+
+markov(args.filenames)
